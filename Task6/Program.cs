812e16ada9719c54deaa75d064fa079a34296683
Kate <c.chistova2014@yandex.ru>
2022-10-17 23:14:45 +0300

    Четное - нечетное число

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.Write("Вы ввели четное число! :)");
+}
+else
+{
+    Console.Write("Вы ввели нечетное число! :)");
+}
