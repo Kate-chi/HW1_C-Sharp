b8788b6fa449292a1d1978d605296fe209c6478a
Kate <c.chistova2014@yandex.ru>
2022-10-17 22:57:11 +0300

    Максимальное из двух чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.Write("Максимальное число: " + num1 + ", минимальное число: " + num2);
+}
+else if(num1 < num2)
+{
+    Console.Write("Максимальное число: " + num2 + ", минимальное число: " + num1);
+}
+else 
+{
+    Console.Write("Упс, Вы ввели одинаковые числа :(");
+}
